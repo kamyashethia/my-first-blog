1cf0ba39629cf474f4e34dca02cd2b5da5edcf52
kamya <kamya.shethia@girlswhocode.com>
2017-07-04 15:16:38 -0700

    My Django Girls app, first commit

---
+++ pythonstuff/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kamyashethia/Documents/GWC_Summer_2017/pythonstuff/pythonstuff/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
